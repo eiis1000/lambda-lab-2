9df8f26595f92320d09a7413a65d3f7731813b72
eiis1000 <erez.m.israeli@gmail.com>
2020-05-29 10:20:57 -0400

    Create Lambda.executeWith

--- src/core/Expression.java
+++ src/core/Expression.java
@@ -1,5 +1,5 @@
 package core;
 
 public interface Expression {
-	void substitute(Variable variable, Expression expression);
+	Expression substitute(Variable variable, Expression expression);
 }
--- src/core/Lambda.java
+++ src/core/Lambda.java
@@ -1,4 +1,7 @@
 package core;
 
 public class Lambda {
+	public Expression executeWith(Expression expression) {
+		throw new UnsupportedOperationException("oh no");
+	}
 }
