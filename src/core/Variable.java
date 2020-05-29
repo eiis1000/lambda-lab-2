1335c36045732f46f68cd0ca4cf16b3ae793bd74
eiis1000 <erez.m.israeli@gmail.com>
2020-05-29 10:29:59 -0400

    Implement Variable class

--- src/core/Variable.java
+++ src/core/Variable.java
@@ -1,4 +1,26 @@
 package core;
 
-public class Variable {
+public class Variable implements Expression {
+
+	private String name;
+	private int boundHash;
+
+	public Expression substitute(Variable variable, Expression expression) {
+		if (this.equals(variable))
+			return expression;
+		else
+			return this;
+	}
+
+	public boolean equals(Object that) {
+		if (that instanceof Variable)
+			return name.equals(((Variable) that).name) && boundHash == ((Variable) that).boundHash;
+		else
+			return false;
+	}
+
+	public int hashCode() {
+		return name.hashCode() + Integer.hashCode(boundHash);
+	}
+
 }
