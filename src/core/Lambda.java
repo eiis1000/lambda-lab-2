d0f7834e244f08ff80bc1aae04826630d29fdd55
eiis1000 <erez.m.israeli@gmail.com>
2020-05-29 10:38:57 -0400

    Implement Lambda

--- src/core/Lambda.java
+++ src/core/Lambda.java
@@ -1,15 +1,28 @@
 package core;
 
-public class Lambda implements Expression{
+public class Lambda implements Expression {
 
-	private Variable boundVariable;
-	private Expression innerExpression;
+	private final Variable boundVariable;
+	private final Expression innerExpression;
+
+	public Lambda(Variable boundVariable, Expression innerExpression) {
+		this.boundVariable = boundVariable;
+		this.innerExpression = innerExpression;
+	}
 
 	public Expression executeWith(Expression toSubstitute) {
 		return innerExpression.substitute(boundVariable, toSubstitute);
 	}
 
 	public Expression substitute(Variable variable, Expression expression) {
-		throw new UnsupportedOperationException();
+		if (variable.equals(boundVariable))
+			return alphaConvert(variable).substitute(variable, expression);
+		else
+			return new Lambda(boundVariable, innerExpression.substitute(variable, expression));
+	}
+
+	public Lambda alphaConvert(Variable variable) {
+		Variable newBound = new Variable(variable.toString() + "1", hashCode());
+		return new Lambda(newBound, innerExpression.substitute(variable, newBound));
 	}
 }
