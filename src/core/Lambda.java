c204b4a09cfc24788811614bdd9a361d50b337ce
eiis1000 <erez.m.israeli@gmail.com>
2020-05-29 10:28:09 -0400

    Make Lambda implement Expression

--- src/core/Lambda.java
+++ src/core/Lambda.java
@@ -1,11 +1,15 @@
 package core;
 
-public class Lambda {
+public class Lambda implements Expression{
 
 	private Variable boundVariable;
 	private Expression innerExpression;
 
 	public Expression executeWith(Expression toSubstitute) {
 		return innerExpression.substitute(boundVariable, toSubstitute);
+	}
+
+	public Expression substitute(Variable variable, Expression expression) {
+		throw new UnsupportedOperationException();
 	}
 }
