91b368f4dda627210738c014df0535b69b1ead45
Autumn Shin <ashin1105@gmail.com>
2020-06-02 00:16:24 -0400

    Application

--- src/core/Application.java
+++ src/core/Application.java
@@ -1,4 +1,30 @@
 package core;
 
-public class Application {
+public class Application implements Expression {
+    public final Expression left;
+    public final Expression right;
+
+    public Application(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public Expression execute() {
+        if(left instanceof Lambda) {
+            return ((Lambda) left).executeWith(right);
+        }
+        else {
+            return this;
+        }
+
+    }
+
+    public Expression substitute(Expression left, Expression right) {
+        if (left.equals(this.left) )
+
+    }
+
+    public String toString() {
+        return "(" + left + " " + right + ")";
+    }
 }
