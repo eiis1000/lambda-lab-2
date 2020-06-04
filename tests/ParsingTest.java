3fc126296b7b31a99b041fde588b66403deab828
eiis1000 <erez.m.israeli@gmail.com>
2020-06-04 10:40:08 -0400

    Write some parsing tests

--- tests/ParsingTest.java
+++ tests/ParsingTest.java
@@ -20,4 +20,19 @@ public class ParsingTest {
 	public void parser1() {
 		assertEquals("x", LambdaParser.parseExpression("x").toString());
 	}
+
+	@Test
+	public void parser2() {
+		assertEquals("(a b)", LambdaParser.parseExpression("a b").toString());
+	}
+
+	@Test
+	public void parser3() {
+		assertEquals("((a b) c)", LambdaParser.parseExpression("a b c").toString());
+	}
+
+	@Test
+	public void parser4() {
+		assertEquals("λx.x", LambdaParser.parseExpression("\\x.x").toString());
+	}
 }
