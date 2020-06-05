5275c8dbfff408a36a31847cfa536e8d548a5743
eiis1000 <erez.m.israeli@gmail.com>
2020-06-05 10:22:12 -0400

    Create CLI

---
+++ src/parsing/CLI.java
@@ -0,0 +1,24 @@
+package parsing;
+
+import java.util.Scanner;
+
+public class CLI {
+	@SuppressWarnings("UnnecessaryContinue")
+	public static void main(String[] args) {
+		System.out.println("Welcome to CASprzak. Run 'help' for a command list, or 'demo' for a tutorial.");
+		Scanner scan = new Scanner(System.in);
+		while (true) {
+			System.out.print("> ");
+			String input = scan.nextLine().strip();
+			if  (input.length() == 0)
+				continue;
+			else if (input.length() >= 4 && "exit".equals(input.substring(0, 4)))
+				break;
+			else {
+				input = input.replaceAll(";.*", "");
+				System.out.println(LambdaParser.parseExpression(input));
+			}
+		}
+		System.out.println("Goodbye!");
+	}
+}
