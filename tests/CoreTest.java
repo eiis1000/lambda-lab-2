2f569f939d67e47e84f4c449f5ac393575064a9a
Autumn Shin <ashin1105@gmail.com>
2020-06-15 22:19:08 -0400

    Add manualTest

--- tests/CoreTest.java
+++ tests/CoreTest.java
@@ -30,15 +30,27 @@ public class CoreTest {
 
     @Test
     public void manualPred() {
-        Expression pred = new Lambda(new Variable("n"), new Lambda(new Variable("f"), new Lambda(new Variable("x"), new Application( new Application(new Lambda(new Variable("g"), new Lambda(new Variable("h"), new Application(new Variable("h"), new Application(new Variable("g"), new Variable("f"))))), new Lambda(new Variable("u"), new Variable("x"))), new Lambda(new Variable("u"), new Variable("u"))))));
+        Expression pred = new Lambda(new Variable("n"), new Lambda(new Variable("f"), new Lambda(new Variable("x"), new Application(new Application(new Lambda(new Variable("g"), new Lambda(new Variable("h"), new Application(new Variable("h"), new Application(new Variable("g"), new Variable("f"))))), new Lambda(new Variable("u"), new Variable("x"))), new Lambda(new Variable("u"), new Variable("u"))))));
     }
 
     @Test
     public void manualLogic() {
-        Expression TRUE=new Lambda(new Variable("x"), new Lambda(new Variable("y"), new Variable("x")));
-        Expression FALSE=new Lambda(new Variable("x"), new Lambda(new Variable("y"), new Variable("y")));
-        Expression AND=new Lambda(new Variable("p"), new Lambda(new Variable("q"), new Application(new Application(new Variable("p"), new Variable("q")), new Variable("p"))));
-        Expression OR=new Lambda(new Variable("p"), new Lambda(new Variable("q"), new Application(new Application(new Variable("p"), new Variable("p")), new Variable("q"))));
-        Expression NOT=new Lambda(new Variable("p"), new Application(new Application(new Variable("p"), FALSE), TRUE));
+        Expression TRUE = new Lambda(new Variable("x"), new Lambda(new Variable("y"), new Variable("x")));
+        Expression FALSE = new Lambda(new Variable("x"), new Lambda(new Variable("y"), new Variable("y")));
+        Expression AND = new Lambda(new Variable("p"), new Lambda(new Variable("q"), new Application(new Application(new Variable("p"), new Variable("q")), new Variable("p"))));
+        Expression OR = new Lambda(new Variable("p"), new Lambda(new Variable("q"), new Application(new Application(new Variable("p"), new Variable("p")), new Variable("q"))));
+        Expression NOT = new Lambda(new Variable("p"), new Application(new Application(new Variable("p"), FALSE), TRUE));
+    }
+
+    @Test
+    public void manualTest() {
+        Expression e0 = new Lambda(new Variable("f"), new Lambda(new Variable("x"), new Variable("x")));
+        Expression pred = new Lambda(new Variable("n"), new Lambda(new Variable("f"), new Lambda(new Variable("x"), new Application(new Application(new Lambda(new Variable("g"), new Lambda(new Variable("h"), new Application(new Variable("h"), new Application(new Variable("g"), new Variable("f"))))), new Lambda(new Variable("u"), new Variable("x"))), new Lambda(new Variable("u"), new Variable("u"))))));
+        Expression TRUE = new Lambda(new Variable("x"), new Lambda(new Variable("y"), new Variable("x")));
+        Expression FALSE = new Lambda(new Variable("x"), new Lambda(new Variable("y"), new Variable("y")));
+        Expression IF = new Lambda(new Variable("p"), new Lambda(new Variable("t"), new Lambda(new Variable("f"), new Application(new Application(new Variable("p"), new Variable("t")), new Variable("f")))));
+        Expression Y = new Lambda(new Variable("f"), new Application(new Lambda(new Variable("x"), new Application(new Variable("f"), new Application(new Variable("x"), new Variable("x")))), new Lambda(new Variable("x"), new Application(new Variable("f"), new Application(new Variable("x"), new Variable("x"))))));
+        Expression ZERO = new Lambda(new Variable("j"), new Application(new Application(new Variable("j"), new Lambda(new Variable("k"), FALSE)), TRUE));
+        Expression TEST = new Application(Y, new Lambda(new Variable("w"), new Lambda(new Variable("x"), new Application(new Application(new Application(IF, new Application(ZERO, new Variable("x"))), e0), new Application(new Variable("w"), new Application(pred, new Variable("x")))))));
     }
 }
